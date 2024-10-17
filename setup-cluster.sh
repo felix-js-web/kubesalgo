5728279f56b8047094127b600a25ba9067d254ca
FelixA <felix.java.web@gmail.com>
2024-10-17 13:22:56 +1100

    working stuff

---
+++ setup-cluster.sh
@@ -0,0 +1,27 @@
+# Delete the KIND cluster
+kind delete cluster --name my-cluster
+
+# Create the KIND cluster
+kind create cluster --config kubernetes/kind-cluster.yaml --name my-cluster
+
+
+# Install Helm
+if ! command -v helm &> /dev/null
+then
+    echo "Helm could not be found, installing..."
+    brew install helm
+fi
+
+# Update Helm dependencies
+helm dependency update ./helm/postgres
+
+# Deploy PostgreSQL using Helm with specified options
+helm upgrade -i my-postgres ./helm/postgres \
+--namespace my-namespace --create-namespace \
+-f ./helm/postgres/values.yaml
+
+echo "PostgreSQL has been installed and is running."
+
+# Verify installation
+kubectl get pods --namespace my-namespace
+kubectl get svc --namespace my-namespace
